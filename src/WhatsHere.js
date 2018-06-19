a05995b5e69f8896576ed46b6914fb114a2a3754
Nolan Magee <nolanmagee@gmail.com>
2018-06-19 14:01:12 -0700

    Added main app wrapped in Google API Wrapper

--- src/WhatsHere.js
+++ src/WhatsHere.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-
+import {GoogleApiWrapper} from 'google-maps-react';
+import MapContainer from './MapContainer';
 
 class WhatsHere extends Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
   }
 
-
   render(){
-    return(
+    return(<div>
       <h1>"What's Going on Here?"</h1>
+      <h3> Using Google Maps API and React </h3>
+      <MapContainer google={this.props.google} />
+      </div>
     )
   }
 }
 
 
-export default WhatsHere;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCkVz1YghzlOOPOz9Hs8oOsNBllvDJW2Ls'
+})(WhatsHere);
